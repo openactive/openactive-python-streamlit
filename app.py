cdc9bcb2e74c67d2ace2c583dbeac2313d43e3c6
Darren Temple <darren.temple@theodi.org>
2024-02-05 20:03:02 +0000

    - Added time filter - Added more items to clearing to ensure that the session state is clearly known at any one time - Reordered table to match filters - Tweaked some nomenclature

--- app.py
+++ app.py
@@ -46,17 +46,46 @@ def clear_outputs():
     st.session_state.opportunities = None
     st.session_state.df = None
     st.session_state.df_edited = None
+    st.session_state.df_filtered = None
+    st.session_state.unique_ids = []
+    st.session_state.unique_superevent_ids = []
+    st.session_state.unique_organizers = []
+    st.session_state.unique_names = []
+    st.session_state.unique_addresses = []
+    st.session_state.unique_times = []
+    st.session_state.unique_times_range = ()
     clear_filters()
 
 def clear_filters():
     st.session_state.filtered_ids = []
     st.session_state.filtered_superevent_ids = []
+    st.session_state.filtered_organizers = []
     st.session_state.filtered_names = []
     st.session_state.filtered_addresses = []
-    st.session_state.filtered_organizers = []
+    st.session_state.filtered_times_range = st.session_state.unique_times_range
 
-def disabled(default=False):
+def disable_input_controls(default=False):
     return True if st.session_state.running else default
+
+def disable_button_clear_filters():
+    filtered_multiselects_active = len(
+        st.session_state.filtered_ids +
+        st.session_state.filtered_superevent_ids +
+        st.session_state.filtered_organizers +
+        st.session_state.filtered_names +
+        st.session_state.filtered_addresses
+    ) > 0
+
+    if (len(st.session_state.filtered_times_range)==0):
+        filtered_times_range_active = False
+    elif (len(st.session_state.filtered_times_range)==1):
+        filtered_times_range_active = True
+    elif (len(st.session_state.filtered_times_range)==2):
+        filtered_times_range_active = \
+            st.session_state.filtered_times_range[0]!=st.session_state.unique_times_range[0] \
+        or  st.session_state.filtered_times_range[1]!=st.session_state.unique_times_range[1]
+
+    return not filtered_multiselects_active and not filtered_times_range_active
 
 def set_address(address_in):
     address_out = ''
@@ -86,7 +115,7 @@ with st.sidebar:
         format_func=lambda x: x[1],
         index=None,
         on_change=clear_outputs,
-        disabled=disabled(),
+        disabled=disable_input_controls(),
     )
     st.selectbox(
         'Data Type',
@@ -95,7 +124,7 @@ with st.sidebar:
         format_func=lambda x: x.split('/')[-1],
         index=None,
         on_change=clear_outputs,
-        disabled=disabled(st.session_state.dataset_url_name==None),
+        disabled=disable_input_controls(st.session_state.dataset_url_name==None),
     )
     col1, col2, col3 = st.columns([1,2,2])
     with col1:
@@ -105,7 +134,7 @@ with st.sidebar:
             on_click=go,
             args=[st.session_state.feed_url],
             # type='primary', # Making this primary gives a little flicker when clicked due to the change in disabled state
-            disabled=disabled(st.session_state.feed_url==None),
+            disabled=disable_input_controls(st.session_state.feed_url==None),
         )
     with col2:
         st.button(
@@ -134,13 +163,13 @@ if (st.session_state.running):
             data = {
                 'ID': st.session_state.opportunities['items'].keys(),
                 'Super-event ID': [''] * num_items,
+                'Organizer name': [''] * num_items,
+                'Organizer logo': [''] * num_items,
                 'Name': [''] * num_items,
                 'Address': [''] * num_items,
                 'Coordinates': [''] * num_items,
                 'Time start': [''] * num_items,
                 'Time end': [''] * num_items,
-                'Organizer name': [''] * num_items,
-                'Organizer logo': [''] * num_items,
                 'URL': [''] * num_items,
             }
 
@@ -148,6 +177,14 @@ if (st.session_state.running):
                 if ('data' in item.keys()):
                     try: data['Super-event ID'][item_idx] = item['data']['superEvent'].split('/')[-1]
                     except: pass
+                    try: data['Organizer name'][item_idx] = item['data']['organizer']['name']
+                    except:
+                        try: data['Organizer name'][item_idx] = item['data']['superEvent']['organizer']['name']
+                        except: pass
+                    try: data['Organizer logo'][item_idx] = item['data']['organizer']['logo']['url']
+                    except:
+                        try: data['Organizer logo'][item_idx] = item['data']['superEvent']['organizer']['logo']['url']
+                        except: pass
                     try: data['Name'][item_idx] = item['data']['name']
                     except: pass
                     try: data['Address'][item_idx] = item['data']['location']['address']
@@ -158,14 +195,6 @@ if (st.session_state.running):
                     except: pass
                     try: data['Time end'][item_idx] = item['data']['endDate']
                     except: pass
-                    try: data['Organizer name'][item_idx] = item['data']['organizer']['name']
-                    except:
-                        try: data['Organizer name'][item_idx] = item['data']['superEvent']['organizer']['name']
-                        except: pass
-                    try: data['Organizer logo'][item_idx] = item['data']['organizer']['logo']['url']
-                    except:
-                        try: data['Organizer logo'][item_idx] = item['data']['superEvent']['organizer']['logo']['url']
-                        except: pass
                     try: data['URL'][item_idx] = item['data']['url']
                     except: pass
 
@@ -179,9 +208,15 @@ if (st.session_state.running):
 
             st.session_state.unique_ids = [id.strip() for id in sorted(set(st.session_state.df['ID'])) if id.strip()]
             st.session_state.unique_superevent_ids = [superevent_id.strip() for superevent_id in sorted(set(st.session_state.df['Super-event ID'])) if superevent_id.strip()]
+            st.session_state.unique_organizers = [organizer.strip() for organizer in sorted(set(st.session_state.df['Organizer name'])) if organizer.strip()]
             st.session_state.unique_names = [name.strip() for name in sorted(set(st.session_state.df['Name'])) if name.strip()]
             st.session_state.unique_addresses = [address.strip() for address in sorted(set(st.session_state.df['Address'])) if address.strip()]
-            st.session_state.unique_organizers = [organizer.strip() for organizer in sorted(set(st.session_state.df['Organizer name'])) if organizer.strip()]
+            st.session_state.unique_times = [time.strip() for time in sorted(set(pd.concat([st.session_state.df['Time start'], st.session_state.df['Time end']]))) if time.strip()]
+            st.session_state.unique_times_range = (
+                datetime.fromisoformat(st.session_state.unique_times[0]).date(),
+                datetime.fromisoformat(st.session_state.unique_times[-1]).date()
+            ) if st.session_state.unique_times else ()
+            st.session_state.filtered_times_range = st.session_state.unique_times_range # We need to initialise this here, for some reason it doesn't do it when the associated filter widget is created
 
             st.session_state.disabled_columns = ['_index'] + list(st.session_state.df.columns) # The index column is not editable by default, but for some reason becomes editable when a filter selection is made, so we explicitly add it here to ensure against this
             st.session_state.disabled_columns.remove('JSON')
@@ -206,6 +241,12 @@ if (st.session_state.opportunities):
             disabled=len(st.session_state.unique_superevent_ids)==0,
         )
         st.multiselect(
+            'Organizer',
+            st.session_state.unique_organizers,
+            key='filtered_organizers',
+            disabled=len(st.session_state.unique_organizers)==0,
+        )
+        st.multiselect(
             'Name',
             st.session_state.unique_names,
             key='filtered_names',
@@ -217,23 +258,19 @@ if (st.session_state.opportunities):
             key='filtered_addresses',
             disabled=len(st.session_state.unique_addresses)==0,
         )
-        st.multiselect(
-            'Organizer',
-            st.session_state.unique_organizers,
-            key='filtered_organizers',
-            disabled=len(st.session_state.unique_organizers)==0,
+        st.date_input(
+            'Time',
+            value=st.session_state.unique_times_range,
+            min_value=st.session_state.unique_times_range[0] if st.session_state.unique_times_range else datetime.now().date(),
+            max_value=st.session_state.unique_times_range[1] if st.session_state.unique_times_range else datetime.now().date(),
+            key='filtered_times_range',
+            disabled=len(st.session_state.unique_times_range)==0,
         )
         st.button(
             'Clear',
             key='button_clear_filters',
             on_click=clear_filters,
-            disabled=len(
-                st.session_state.filtered_ids +
-                st.session_state.filtered_superevent_ids +
-                st.session_state.filtered_names +
-                st.session_state.filtered_addresses +
-                st.session_state.filtered_organizers
-            )==0
+            disabled=disable_button_clear_filters(),
         )
 
     df_filtered = st.session_state.df
@@ -247,6 +284,11 @@ if (st.session_state.opportunities):
         df_filtered = df_filtered.loc[df_filtered['Address'].isin(st.session_state.filtered_addresses)]
     if (st.session_state.filtered_organizers):
         df_filtered = df_filtered.loc[df_filtered['Organizer name'].isin(st.session_state.filtered_organizers)]
+    if (st.session_state.filtered_times_range):
+        df_filtered = df_filtered.loc[
+                (df_filtered['Time start'].apply(datetime.fromisoformat).apply(datetime.date) >= st.session_state.filtered_times_range[0])
+            &   (df_filtered['Time end'].apply(datetime.fromisoformat).apply(datetime.date) <= st.session_state.filtered_times_range[len(st.session_state.filtered_times_range)-1])
+        ]
     if (len(df_filtered)>0):
         df_filtered.at[df_filtered.index[0], 'JSON'] = True
 
@@ -259,6 +301,11 @@ if (st.session_state.opportunities):
             '_index': st.column_config.NumberColumn(label='Index'),
             'JSON': st.column_config.CheckboxColumn(),
             'Organizer logo': st.column_config.ImageColumn(),
+            # It is simpler to leave the time columns as strings rather than convert to DateTime objects, which
+            # has issues when the strings are empty, so the following lines aren't needed but are left here to
+            # warn against a change that may take a while to figure out and ultimately lead to the same conclusion.
+            #   'Time start': st.column_config.DatetimeColumn(format='YYYY-MM-DD HH:mm'),
+            #   'Time end': st.column_config.DatetimeColumn(format='YYYY-MM-DD HH:mm'),
             'URL': st.column_config.LinkColumn(),
         },
     )
